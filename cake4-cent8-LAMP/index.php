d50cb4f69f25d8801fb98e75e0fda361d114b33e
tamago-bo-ro <syam445.momiji@gmail.com>
2020-10-12 23:07:32 +0900

    [Add] cent8 cake4 LAMP

---
+++ cake4-cent8-LAMP/index.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The Front Controller for handling every request
+ *
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.2.9
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
+ */
+
+// Check platform requirements
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+if (!defined('REQ_PATH')) {
+    define('REQ_PATH', dirname(__DIR__) . DS . 'cakephp');
+}
+
+// Check platform requirements
+require REQ_PATH . '/config/requirements.php';
+
+// For built-in server
+if (PHP_SAPI === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+        return false;
+    }
+}
+require REQ_PATH . '/vendor/autoload.php';
+
+use App\Application;
+use Cake\Http\Server;
+
+// Bind your application to the server.
+$server = new Server(new Application(REQ_PATH . '/config'));
+
+// Run the request/response through the application and emit the response.
+$server->emit($server->run());
